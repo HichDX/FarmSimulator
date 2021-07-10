5872902d8442b026b9e32d192ad4059c89efeaf7
HichDX <hichdx@gmail.com>
2021-07-10 13:11:06 +0700

    Update

--- Factory/main.php
+++ Factory/main.php
@@ -8,4 +8,17 @@ $countCow = 10; // Начальное количество коров
 $countChicken = 20; // Начальное количество кур
 $farm = new Farm();
 $farm->factory($countCow, $countChicken);
-$farm->factory(5, 1);
+
+echo
+"-----------------------------------------------------
+                    Сходить в магазин?
+-----------------------------------------------------\n";
+
+$answer = strtoupper(readline("Y[Да] N[Нет] = "));
+if($answer === "Y"){
+    $countCow = readline("Сколько коров купить? ");
+    $countChicken = readline("Сколько кур купить? ");
+    $farm->factory($countCow, $countChicken);
+}else{
+    echo "Конец";
+}
